8e82070b97069a3155af2082d75cdea0b0746566
Woodhouse <pariswoodhouse@hotmail.com>
2022-01-11 21:15:59 -0800

    open and close buttons for modal

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+// Grabbing DOM Elements
+const addBookButton = document.querySelector('.add-book-button');
+const modalContainer = document.querySelector('.modal-container');
+const exitModalButton = document.querySelector('.exit-modal');
+const body = document.querySelector('body')
+
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -7,3 +13,13 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
+addBookButton.addEventListener('click', () => {
+  modalContainer.style.display = 'flex';
+  body.style.opacity = '.5' 
+  modalContainer.style.opacity = '1'
+  
+});
+
+exitModalButton.addEventListener('click', () => {
+  modalContainer.style.display = 'none';
+});
