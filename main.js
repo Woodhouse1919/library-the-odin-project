ed6adfb0301c667c8b3e04c66169e5dd558d87d1
Woodhouse <pariswoodhouse@hotmail.com>
2022-01-12 19:01:21 -0800

    Created addbook function

--- main.js
+++ main.js
@@ -2,9 +2,17 @@
 const addBookButton = document.querySelector('.add-book-button');
 const modalContainer = document.querySelector('.modal-container');
 const exitModalButton = document.querySelector('.exit-modal');
-const body = document.querySelector('body')
 
-let myLibrary = [];
+
+let myLibrary = [
+  {
+    title: 'The Hobbit',
+    author: 'JRR Tolkien',
+    pages: '300',
+    read: true,
+  }
+
+];
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -13,13 +21,50 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
+function addBook(title, author, pages, read) {
+// Grabbing table Dom Element
+const table = document.querySelector('tbody')
+const tableRow = document.createElement('tr')
+const tableTitle = document.createElement('td')
+const tableAuthor = document.createElement('td')
+const tablePages = document.createElement('td')
+const tableRead = document.createElement('td')
+const checkBox = document.createElement('input')
+
+
+tableRow.classList.add('book')
+table.appendChild(tableRow)
+
+tableTitle.classList.add('title')
+tableTitle.textContent = title
+tableRow.appendChild(tableTitle)
+
+tableAuthor.classList.add('author')
+tableAuthor.textContent = author
+tableRow.appendChild(tableAuthor)
+
+tablePages.classList.add('pages')
+tablePages.textContent = pages
+tableRow.appendChild(tablePages)
+
+
+checkBox.type = 'checkbox'
+checkBox.name = 'read'
+checkBox.id = 'read'
+checkBox.checked = read
+tableRow.appendChild(tableRead)
+tableRead.appendChild(checkBox)
+}
+
+
+
+// Event listener for opening Book Info Model
 addBookButton.addEventListener('click', () => {
   modalContainer.style.display = 'flex';
-  body.style.opacity = '.5' 
-  modalContainer.style.opacity = '1'
   
 });
 
+// Event listener for closing Book Info Modal
 exitModalButton.addEventListener('click', () => {
   modalContainer.style.display = 'none';
 });
